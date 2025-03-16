a431c5be4e4913fbac7d58748d7d2593e36cd485
Caio Arrabal <caio.a.1998@gmail.com>
2025-03-16 13:59:42 -0300

    Sync

--- notebooks/Utils/DuckDb.py
+++ notebooks/Utils/DuckDb.py
@@ -16,3 +16,26 @@ class DuckDb(object):
         '''
         df = conn.execute(query).fetch_df()
         return df
+    
+
+    def get_total_city_count(self) -> int:
+        return self.__get_total_category_count('city', project_config.DUCKDB_MEMBERS_DATABASE)
+
+
+    def get_total_registered_via_count(self) -> int:
+        return self.__get_total_category_count('registered_via', project_config.DUCKDB_MEMBERS_DATABASE)
+
+
+    def get_total_payment_method_id_count(self) -> int:
+        return self.__get_total_category_count('payment_method_id', project_config.DUCKDB_TRANSACTIONS_DATABASE)
+
+
+    def __get_total_category_count(self, column: str, table: str) -> int:
+        conn = self.get_connection()
+        query = f'''
+            SELECT
+                COUNT(DISTINCT({column}))
+            FROM {table}
+        '''
+        result = conn.execute(query).fetchone()
+        return result[0]
--- notebooks/Utils/PandasNotebookConfigs.py
+++ notebooks/Utils/PandasNotebookConfigs.py
@@ -4,5 +4,5 @@ import pandas as pd
 def config():
     # PANDAS CONFIGS
     # Disable scientific notation
-    print(f'Ajustando display.float.format para %.2f')
-    pd.set_option('display.float_format', lambda x: '%.2f' % x)
+    print(f'Ajustando display.float.format para %.4f')
+    pd.set_option('display.float_format', lambda x: '%.4f' % x)
